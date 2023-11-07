f49cdbbc1aaee36b5fb9cf5e834ff4f7bd93f257
prayTiger <150120934+prayTiger@users.noreply.github.com>
2023-11-07 16:33:55 +0800

    laternjson

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var animation = bodymovin.loadAnimation({
     rederer:'svg',
     loop: true,
     autoplay: true,
-    path: 'https://raw.githubusercontent.com/LiJing1219/TigerGod/main/001.json'
+    path: 'https://github.com/prayTiger/prayTiger/blob/acb3641871bc396368690164e573c1be2604b826/lanterns.json'
 
 });
 
