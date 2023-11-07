acb3641871bc396368690164e573c1be2604b826
prayTiger <150120934+prayTiger@users.noreply.github.com>
2023-11-07 16:22:58 +0800

    origin

---
+++ script.js
@@ -0,0 +1,11 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lantern1'),
+    rederer:'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://raw.githubusercontent.com/LiJing1219/TigerGod/main/001.json'
+
+});
+
+
+
