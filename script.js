06a5171af9ffd0d250fb5abd270f3d9e6d6e187e
prayTiger <150120934+prayTiger@users.noreply.github.com>
2023-12-24 22:31:00 +0800

    1224
    - 增加各頁面動態
    - 動畫播放方式修改
    - 增加logo動態效果
    - 更換返回鈕

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('lantern'),
-    rederer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'https://raw.githubusercontent.com/prayTiger/Json/main/lanterns.json'
 
-});
 
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('index_bg'),
@@ -16,40 +9,67 @@ var animation = bodymovin.loadAnimation({
 
 });
 
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('test'),
+    rederer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi5-1.json'
+
+});
+
 /*土地公*/
 
 
 
+var animation = lottie.loadAnimation({
+    container: document.getElementById('tudi_bg'),
+    rederer: 'svg',
+    loop: true,
+    autoplay: true,
+    name: 'tudi4',
+    path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi4.json'
+});
 
-
-let page = 0;
+let click = 0;
 function Previous() {
     // lottie.destroy();
-    page--;
-    console.log("page=%d", page);
+    click--;
+    console.log("click=%d", click);
     switchPage();
 }
     
 function Next() {
     // lottie.destroy();
-    page++;
-    console.log("page=%d", page);
-    
+    click++;
+    console.log("click=%d", click);
     switchPage();
 }
 
 function switchPage(){
-    switch(page){
+    switch(click){
         case 0 :{
             lottie.destroy('tudi4-2');
-            
-            
             break;
         }
         case 1 :{
-            lottie.destroy('tudi4-3');
+            // lottie.destroy('tudi4-3');
             lottie.loadAnimation({
-            container: document.getElementById('tudi4-2'),
+            container: document.getElementById('tudi_main'),
+            rederer: 'svg',
+            loop: false,
+            autoplay: false,
+            name: 'tudi4-1',
+            path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi4-1.json'
+            });
+            lottie.play(); 
+
+            break;
+        }
+        case 2 :{
+            lottie.destroy('tudi4-1');
+            lottie.loadAnimation({
+            container: document.getElementById('tudi_main'),
             rederer: 'svg',
             loop: false,
             autoplay: false,
@@ -57,108 +77,76 @@ function switchPage(){
             path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi4-2.json'
             });
             lottie.play(); 
-
-            break;
-        }
-        case 2 :{
-            lottie.destroy('tudi4-2');
-            lottie.destroy('tudi5-1');
-            lottie.loadAnimation({
-            container: document.getElementById('tudi4-3'),
-            rederer: 'svg',
-            loop: false,
-            autoplay: false,
-            name: 'tudi4-3',
-            path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi4-3.json'
-            });
-            lottie.play(); 
             break;
         }
         case 3 :{
-            lottie.destroy('tudi4-3');
-            lottie.destroy('tudi5-2');
+            lottie.destroy('tudi4-2');
+            lottie.destroy('tudi4');
             lottie.loadAnimation({
-            container: document.getElementById('tudi5-1'),
+            container: document.getElementById('tudi_bg'),
             rederer: 'svg',
             loop: false,
             autoplay: false,
-            name: 'tudi5-1',
-            path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi5-1.json'
+            name: 'tudi5',
+            path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi5.json'
             });
             lottie.play(); 
+            lottie.loadAnimation({
+                container: document.getElementById('tudi_main'),
+                rederer: 'svg',
+                loop: false,
+                autoplay: false,
+                name: 'tudi5-1',
+                path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi5-1.json'
+                });
+                lottie.play(); 
+                break;
             break;
         }
         case 4 :{
-            lottie.destroy('tudi5-1');
-            lottie.destroy('tudi5-2');
-            lottie.loadAnimation({
-            container: document.getElementById('tudi5-2'),
-            rederer: 'svg',
-            loop: false,
-            autoplay: false,
-            name: 'tudi5-2',
-            path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi5-1.json'
-            });
-            lottie.play(); 
-            break;
-        }
-        case 5 :{
-            lottie.destroy('tudi5-2');
             
             
-            break;
+            // lottie.loadAnimation({
+            // container: document.getElementById('tudi_main'),
+            // rederer: 'svg',
+            // loop: false,
+            // autoplay: false,
+            // name: 'tudi5-1',
+            // path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi5-1.json'
+            // });
+            // lottie.play(); 
+            // break;
         }
+        // case 5 :{
+        //     lottie.destroy('tudi5-2');
+            
+            
+        //     break;
+        // }
      }
  }
 
-let container = document.getElementById('tudi4-1')
-let animationCompleted = true;
-    var animation = lottie.loadAnimation({
-        container: container,
-        rederer: 'svg',
-        loop: true,
-        autoplay: true,
-        name: 'tudi4-1',
-        path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi4-1.json'
-    });
-
-    // var play = true
-    // function SwichAndStart(){
-    //     if(play){
-    //         lottie.play();
-    //         play = false;
-    //         console.log("paly=flase");
-    //     }else{
-    //         lottie.destroy('tudi4-2');
-    //         play = true;
-    //         console.log("play=true");
-    //         lottie.loadAnimation({
-    //             container: document.getElementById('tudi4-2'),
-    //             rederer:'svg',
-    //             loop: false,
-    //             autoplay: false,
-    //             name: 'tudi4-3',
-    //             path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi4-3.json'
-    //         });
-    //     }
-    //   };
 
 
 
 
-    // var animation = bodymovin.loadAnimation({
-    //     container: document.getElementById('tudi4-2'),
-    //     rederer: 'svg',
-    //     loop: false,
-    //     autoplay: false,
-    //     name: 'tudi4-2',
-    //     path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi4-2.json'
-    // });
-
-    // var animation = bodymovin.loadAnimation({
-    //     container: document.getElementById('tudi4-3'),
-    //     rederer: 'svg',
-    //     loop: false,
-    //     autoplay: false,
-    //     path: 'https://raw.githubusercontent.com/prayTiger/Json/main/tudi4-3.json'
-    // });
+/*淡出效果*/
+    window.addEventListener('scroll', reveal);
+  
+    function reveal(){
+        var reveals = document.querySelectorAll('.reveal');
+  
+        for(var i = 0; i < reveals.length; i++){
+  
+          var windowheight = window.innerHeight;
+          var revealtop = reveals[i].getBoundingClientRect().top;
+          var revealpoint = 150;
+  
+          if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+          }
+          else{
+            reveals[i].classList.remove('active');
+          }
+        }
+      }
